b8753c68b40797cfce4cb0b7e6d6269934342393
J Fray <jermainev.fray@gmail.com>
2017-07-26 18:45:23 -0400

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,12 @@
+print("Hello My Friend!!!!!")
+
+
+a = input("What is your name?")
+print("Nice to meet you", a)
+b = input("What is your favorite color?")
+print(b,"?, That is a great color!!!")
+c = input("What's your favorite food?")
+print(c, "sounds yummy")
+d = input("Do you have to go? yes or no?")
+
+
