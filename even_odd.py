69ffc006bc16883dea6d4f08ac14111f3cbabbc8
J Fray <jermainev.fray@gmail.com>
2017-07-26 18:45:15 -0400

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+print("Is your number even or odd??")
+print("I will tell you !!!!")
+
+a = int(input("What number do you want to examine???"))
+
+if a % 2 == 0:
+    print("That number is even")
+else:
+    print("That is an odd number")
