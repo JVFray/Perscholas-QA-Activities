bbc7ac5f9e8044f787574dc5ced4aca4b7aa2f55
J Fray <jermainev.fray@gmail.com>
2017-07-29 14:47:06 -0400

    Update Temperature chart.py

--- Temperature chart.py
+++ Temperature chart.py
@@ -1,7 +1,7 @@
 celsius = 0
 print("C          F")
 while celsius <= 100:
-    fahrenheit = (celsius - 30) * 2
+    fahrenheit = (celsius * 1.8) + 32
     print(celsius, "      ",fahrenheit)
     celsius += 5
 
