a736e9064cec791971b61057d6ba3e0d0356e07d
J Fray <jermainev.fray@gmail.com>
2017-07-29 10:37:29 -0400

    Add files via upload

--- Guessing Game.py
+++ Guessing Game.py
@@ -3,23 +3,33 @@ import random
 guessesTaken = 0
 
 print('Hello! What is your name?')
- myName = input()
+myName = input()
 
- number = random.randint(1, 20)
- print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 10)
+print(number)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 10.')
 
- while guessesTaken < 6:
+while guessesTaken < 3:
     print('Take a guess.') # There are four spaces in front of print.
-     guess = input()
-    guess = int(guess)
+    guess = int(input())
+
+
 
     guessesTaken = guessesTaken + 1
 
-    if guess < number:
-        print('Your guess is too low.') # There are eight spaces in front of print.
-
-    if guess > number:
-         print('Your guess is too high.')
-
     if guess == number:
+        print('You Got It!!!!')
         break
+
+    elif guess == (number - 1) or (guess == number + 1):
+        print('Your HOT!!') # There are eight spaces in front of print.
+
+    elif guess == (number - 2) or guess == (number + 2):
+         print('Your WARM!!')
+
+    else:
+        print('Your cold as Hell')
+
+    if guessesTaken == 3:
+        print('Out of Turns, Your a Failure')
+
