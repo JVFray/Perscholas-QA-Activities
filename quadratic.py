32daa5e297d26c782a5ac87f0e754144cfa29f73
J Fray <jermainev.fray@gmail.com>
2017-07-31 19:08:36 -0400

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -11,5 +11,5 @@ x = int(input('What is the value of X '))
 answer = (a * (x**2) + (b*x) + c)
 
 
-print('The following quadratic was entered', a,'X^2 +',b,'X +', c )
+print('The following quadratic was entered {}X^2 + {}X + {}'.format(a,b,c))
 print('The value of the quadratic is ',answer)
